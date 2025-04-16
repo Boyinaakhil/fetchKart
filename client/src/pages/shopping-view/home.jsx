bd6cbdfb89d9a7cdc6014205e5743faf0a60c81a
Boyinaakhil <323103310035@gvpce.ac.in>
2025-04-17 02:43:19 +0530

    Initial committ

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -53,6 +53,7 @@ function ShoppingHome() {
     (state) => state.shopProduct
   );
   const { featureImageList } = useSelector((state) => state.commonFeature);
+  const { cartItems } = useSelector((state) => state.shopCart);
 
   const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
 
@@ -76,7 +77,25 @@ function ShoppingHome() {
     dispatch(fetchProductDetails(getCurrentProductId));
   }
 
-  function handleAddtoCart(getCurrentProductId) {
+  function handleAddToCart(getCurrentProductId, getTotalStock) {
+    let getCartItems = cartItems.items || [];
+
+    if (getCartItems.length) {
+      const indexOfCurrentItem = getCartItems.findIndex(
+        (item) => item.productId === getCurrentProductId
+      );
+      if (indexOfCurrentItem > -1) {
+        const getQuantity = getCartItems[indexOfCurrentItem].quantity;
+        if (getQuantity + 1 > getTotalStock) {
+          toast({
+            title: `Only ${getQuantity} quantity can be added for this item`,
+            variant: "destructive",
+          });
+
+          return;
+        }
+      }
+    }
     dispatch(
       addToCart({
         userId: user?.id,
@@ -214,7 +233,7 @@ function ShoppingHome() {
                   <ShoppingProductTile
                     handleGetProductDetails={handleGetProductDetails}
                     product={productItem}
-                    handleAddtoCart={handleAddtoCart}
+                    handleAddtoCart={handleAddToCart}
                   />
                 ))
               : null}
